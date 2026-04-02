fe6a72d76c6b1f1add1d65231062b29fb9666720
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 12:52:34 +0200

    WindowConfig helper method

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Configs/ConfigExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace NeuChessHu.Configs;
+
+internal static class ConfigExtensions
+{
+    internal static T WindowConfig<T>(this T window, Action<T> configure) where T : Window
+    {
+        configure(window);
+        return window;
+    }
+}
+
