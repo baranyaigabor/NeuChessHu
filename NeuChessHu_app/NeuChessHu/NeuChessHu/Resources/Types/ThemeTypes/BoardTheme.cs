a63f8872b35510c0f25b6954da3d5665b9611351
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 13:25:09 +0200

    BoardTheme struct

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/Types/ThemeTypes/BoardTheme.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace NeuChessHu.Resources.Types.ThemeTypes;
+
+public readonly struct BoardTheme(string value) : IEquatable<BoardTheme>
+{
+    public string Value { get; } = value ?? throw new ArgumentNullException(nameof(Value));
+    
+    public static BoardTheme PastelGreen { get; } = new("Pastel Green");
+    public static BoardTheme BrightBlue { get; } = new("Bright Blue");
+    public static BoardTheme PastelBlue { get; } = new("Pastel Blue");
+    public static BoardTheme PinkWorld { get; } = new("Pink World");
+    public static BoardTheme Modern { get; } = new("Modern");
+    public static BoardTheme Wooden { get; } = new("Wooden");
+    public static BoardTheme Fradi { get; } = new("Fradi");
+    public static BoardTheme Royal { get; } = new("Royal");
+    public static BoardTheme Death { get; } = new("Death");
+
+    public static readonly Dictionary<string, BoardTheme> AllBoardThemes = typeof(BoardTheme)
+        .GetProperties(BindingFlags.Public | BindingFlags.Static)
+        .Where(x => x.PropertyType == typeof(BoardTheme))
+        .Select(x => (BoardTheme)x.GetValue(null)!)
+        .ToDictionary(x => x.Value);
+
+    public override bool Equals(object? obj) =>
+        obj is BoardTheme other && Equals(other);
+
+    public bool Equals(BoardTheme other) =>
+        string.Equals(Value, other.Value, StringComparison.OrdinalIgnoreCase);
+
+    public override int GetHashCode() =>
+        Value?.GetHashCode(StringComparison.OrdinalIgnoreCase) ?? 0;
+
+    public override string ToString() => Value;
+
+    public static bool operator ==(BoardTheme left, BoardTheme right) =>
+        left.Equals(right);
+
+    public static bool operator !=(BoardTheme left, BoardTheme right) =>
+        !left.Equals(right);
+}
