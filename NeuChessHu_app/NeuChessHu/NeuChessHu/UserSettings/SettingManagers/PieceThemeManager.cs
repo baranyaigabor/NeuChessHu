7132a054e16af3f65957df25d7446b8e0829b0f8
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 19:01:58 +0200

    PieceThemeManager

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/UserSettings/SettingManagers/PieceThemeManager.cs
@@ -0,0 +1,24 @@
+﻿using ChessMechanics.ChessBoard.Definitions;
+using NeuChessHu.Resources.Types.ThemeTypes;
+using Properties;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace NeuChessHu.UserSettings.SettingManagers;
+
+internal static class PieceThemeManager
+{
+    internal static PieceTheme Decode()
+    {
+        if (PieceTheme.AllPieceThemes.TryGetValue(Settings.Default.PieceTheme, out PieceTheme pieceTheme))
+            return pieceTheme;
+
+        throw new NotSupportedException($"{Settings.Default.PieceTheme} is not supported!");
+    }
+
+    internal static BitmapImage ImageLoader(Piece? piece, Side color, BindableSettings settings) => 
+        piece is null 
+            ? throw new ArgumentNullException(nameof(piece))
+            : new (new Uri($"/resources/Themes/PieceThemes/{settings.PieceTheme}/" +
+                $"{color}Pieces/{piece}{color}.png", UriKind.Relative));
+}
