94373778dce466dd62581b2dd29e1cba1876b0c5
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 17:24:15 +0200

    BoardThemes + helper brush converter

--- NeuChessHu_app/NeuChessHu/NeuChessHu/Converters/ColorConverters.cs
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Converters/ColorConverters.cs
@@ -6,4 +6,7 @@ internal static class ColorConverters
 {
     internal static Color ColorFromString(string hexa) =>
         (Color)ColorConverter.ConvertFromString(hexa);
+
+    internal static Brush BrushFromString(string hexa) =>
+        (Brush)new BrushConverter().ConvertFromString(hexa)!;
 }
---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/Themes/BoardThemes/PinkWorldBoard.cs
@@ -0,0 +1,20 @@
+﻿using NeuChessHu.Converters;
+using System.Windows;
+
+namespace NeuChessHu.Resources.Themes.BoardThemes;
+
+internal static class PinkWorldBoard
+{
+    readonly static ResourceDictionary resources = Application.Current.Resources;
+
+    internal static void Set()
+    {
+        resources["LightSquareBrush"] = ColorConverters.BrushFromString("#F4F0F1");
+        resources["DarkSquareBrush"] = ColorConverters.BrushFromString("#E098A4");
+        resources["SelectedLightSquareBrush"] = ColorConverters.BrushFromString("#E6B298");
+        resources["SelectedDarkSquareBrush"] = ColorConverters.BrushFromString("#DC8672");
+        resources["OddBoardIdentifierBrush"] = ColorConverters.BrushFromString("#E098A4");
+        resources["EvenBoardIdentifierBrush"] = ColorConverters.BrushFromString("#F4F0F1");
+        resources["BoardBorderBrush"] = ColorConverters.BrushFromString("#00FFFFFF");
+    }
+}
