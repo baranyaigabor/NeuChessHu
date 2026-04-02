47738ece94e59a52df8e9427ea350a19780b78f8
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 18:48:17 +0200

    LanguageManager

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/UserSettings/SettingManagers/LanguageManager.cs
@@ -0,0 +1,55 @@
+﻿using NeuChessHu.Resources.Images.Register.Images.Dynamics.Localized;
+using NeuChessHu.Resources.Languages;
+using NeuChessHu.Resources.Types;
+using NeuChessHu.Resources.Types.ThemeTypes;
+using Properties;
+using System.Globalization;
+using System.Reflection;
+
+namespace NeuChessHu.UserSettings.SettingManagers;
+
+internal static class LanguageManager
+{
+    static readonly Dictionary<Language, Action> LanguageActions = new()
+    {
+        {
+            Language.Hungarian,
+            SetHungarian
+        },
+        {
+            Language.English,
+            SetEnglish
+        }
+    };
+
+    static void SetHungarian()
+    {
+        HungarianDictionary.SetLanguage();
+        LocalizedImages.SetLanguage(Language.Hungarian);
+    }
+
+    static void SetEnglish()
+    {
+        EnglishDictionary.SetLanguage();
+        LocalizedImages.SetLanguage(Language.English);
+    }
+
+    internal static Language Decode()
+    {
+        if (Language.AllLanguages.TryGetValue(Settings.Default.Language, out Language language))
+            return language;
+
+        throw new NotSupportedException($"{Settings.Default.Language} is not supported!");
+    }
+
+    internal static void ApplyLanguage(Language language)
+    {
+        if (language == Language.System)
+            language = IsWindowsHungarian() ? Language.Hungarian : Language.English;
+
+        LanguageActions[language].Invoke();
+    }
+
+    static bool IsWindowsHungarian() =>
+        CultureInfo.InstalledUICulture.TwoLetterISOLanguageName.Equals("hu", StringComparison.OrdinalIgnoreCase);
+}
