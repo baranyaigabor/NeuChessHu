a2af5ff75ec7f6e1d2e25f2f59d253d1c0ab6c0b
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 16:09:34 +0200

    AppResources helper for resource dictionary

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/AppResources.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace NeuChessHu.Resources;
+
+internal static class AppResources
+{
+    internal static T Get<T>(string key)
+    {
+        if (!Application.Current.Resources.Contains(key))
+            throw new ResourceReferenceKeyNotFoundException(key, Application.Current);
+
+        if (Application.Current.Resources[key] is not T value)
+            throw new InvalidCastException($"Resource '{key}' and its type '{typeof(T).Name}' are not matching");
+
+        return value;
+    }
+}
