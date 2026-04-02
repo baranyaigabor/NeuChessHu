5d2ff849e4df8eb08934ebb4f3497dd9f3f54758
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 18:07:53 +0200

    Hungarian and english dictionary

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/Languages/EnglishDictionary.cs
@@ -0,0 +1,156 @@
+﻿using System.Windows;
+
+namespace NeuChessHu.Resources.Languages;
+
+internal class EnglishDictionary
+{
+    readonly static ResourceDictionary resources = Application.Current.Resources;
+
+    internal static void SetLanguage()
+    {
+        SetCommonTexts();
+        SetMenuSideBarTexts();
+        SetSettingsNames();
+        SetOptionsPopUpTexts();
+        SetLookingForMatchWindowTexts();
+        SetSettingsValues();
+        SetMatchEndWindowTexts();
+        SetMenuPopUpTexts();
+        SetLoginPopUpTexts();
+        SetTimeSetterWindowTexts();
+    }
+
+    static void SetCommonTexts()
+    {
+        resources["GoBackText"] = "Go back";
+        resources["SettingsText"] = "Settings";
+    }
+
+    static void SetMenuSideBarTexts()
+    {
+        resources["StartGameText"] = "Start Game";
+        resources["MoreOptionsText"] = "More";
+        resources["JoinTournamentText"] = "Join Tournament";
+        resources["PlayStockfishText"] = "Play against Stockfish";
+        resources["CustomGameText"] = "Custom Game";
+    }
+
+    static void SetSettingsNames()
+    {
+        resources["AppThemeText"] = "App Theme";
+        resources["BoardThemeText"] = "Board Theme";
+        resources["PieceThemeText"] = "Piece Theme";
+        resources["LanguageText"] = "Language";
+        resources["DisableSoundsText"] = "Disable Sounds";
+        resources["FocusModeText"] = "Focus Mode";
+        resources["AutoQueenText"] = "Auto-Queen";
+        resources["ConfirmEachMoveText"] = "Confirm Each Move";
+    }
+
+    static void SetSettingsValues()
+    {
+        resources["SystemAppTheme"] = "System";
+        resources["DarkAppTheme"] = "Dark";
+        resources["LightAppTheme"] = "Light";
+
+        resources["BrightBlueBoardTheme"] = "Bright Blue";
+        resources["DeathBoardTheme"] = "Death";
+        resources["FradiBoardTheme"] = "Fradi";
+        resources["ModernBoardTheme"] = "Modern Style";
+        resources["PastelBlueBoardTheme"] = "Pastel Blue";
+        resources["PastelGreenBoardTheme"] = "Pastel Green";
+        resources["PinkWorldBoardTheme"] = "Pink World";
+        resources["RoyalBoardTheme"] = "Royal";
+        resources["WoodenBoardTheme"] = "Wooden Style";
+
+        resources["SystemLanguage"] = "System";
+        resources["EnglishLanguage"] = "English";
+        resources["HungarianLanguage"] = "Hungarian";
+    }
+
+    static void SetOptionsPopUpTexts()
+    {
+        resources["AbortText"] = "Abort";
+        resources["ResignText"] = "Resign";
+        resources["DrawText"] = "Offer Draw";
+    }
+
+    static void SetLookingForMatchWindowTexts()
+    {
+        resources["LookingForMatchText"] = "Looking for Match";
+        resources["SearchingNotes"] = new List<string>
+        {
+            "Waiting for a worthy opponent...",
+            "Scanning the horizon for \na challenger...",
+            "Searching for someone \nbrave enough to face you...",
+            "Preparing the battlefield \nfor your next rival...",
+            "Looking for an opponent who \nwon’t resign instantly...",
+            "Seeking a mind equal \nto yours...",
+            "Your next victim \nis being selected...",
+            "Finding a player who knows \nwhat they’re getting into...",
+            "The algorithm whispers: \na challenger approaches",
+            "Your adversary is almost ready",
+            "Searching for opponent…",
+            "Hunting for a challenger…",
+            "Scanning for rival…",
+            "Looking for a brave opponent…",
+            "Preparing the battlefield…",
+            "A challenger approaches…",
+            "Loading opponent…",
+            "Seeking your next target…",
+            "Selecting an opponent…",
+            "Opponent incoming…",
+            "Almost found your rival…",
+            "Matchmaking in progress…"
+        };
+    }
+
+    static void SetMatchEndWindowTexts()
+    {
+        resources["MatchResultLostText"] = "Match Lost!";
+        resources["MatchResultDrawnText"] = "Match Drawn!";
+        resources["MatchResultWonText"] = "Match Won!";
+        resources["MatchAbortedText"] = "Game Aborted!";
+
+        resources["CheckmateText"] = "By Checkmate";
+        resources["TimedoutText"] = "By Timeout";
+        resources["ResignationText"] = "By Resignation";
+
+        resources["MutualAgreementText"] = "By Mutual Agreement";
+        resources["StalemateText"] = "By Stalemate";
+        resources["FiftyConsecutiveMovesTexts"] = "By 50 Consecutive Moves";
+        resources["SeventyFiveConsecutiveMovesTexts"] = "By 75 Consecutive Moves";
+        resources["InsufficentMaterialText"] = "By Insufficent Materials";
+        resources["ThreefoldRepetition"] = "By Threefold-Repetition";
+        resources["FivefoldRepetition"] = "By Fivefold-Repetition";
+
+        resources["PlayAgainButtonText"] = "Play again";
+        resources["QuitToMenuButtonText"] = "Quit to menu";
+    }
+
+    static void SetMenuPopUpTexts()
+    {
+        resources["ProfileSettingsText"] = "Profile Settings";
+        resources["LogoutText"] = "Logout";
+    }
+
+    static void SetLoginPopUpTexts()
+    {
+        resources["LoginText"] = "Login";
+        resources["LoginNotificationText"] = "Please, log in to access this feature!";
+    }
+
+    static void SetTimeSetterWindowTexts()
+    {
+        resources["ChooseTimeText"] = "Choose Time";
+        resources["BulletText"] = "Bullet";
+        resources["BlitzText"] = "Blitz";
+        resources["RapidText"] = "Rapid";
+        resources["BulletOneMinText"] = "1 min";
+        resources["BlitzThreeMinText"] = "3 min";
+        resources["BlitzFiveMinText"] = "5 min";
+        resources["RapidTenMinText"] = "10 min";
+        resources["RapidThirtyMinText"] = "15 min";
+        resources["MinuteText"] = "min";
+    }
+}
---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/Languages/HungarianDictionary.cs
@@ -0,0 +1,155 @@
+﻿using System.Windows;
+
+namespace NeuChessHu.Resources.Languages;
+
+internal class HungarianDictionary
+{
+    readonly static ResourceDictionary resources = Application.Current.Resources;
+    internal static void SetLanguage()
+    {
+        SetCommonTexts();
+        SetMenuSideBarTexts();
+        SetSettingsNames();
+        SetSettingsValues();
+        SetOptionsPopUpTexts();
+        SetLookingForMatchWindowTexts();
+        SetMatchEndWindowTexts();
+        SetMenuPopUpTexts();
+        SetLoginPopUpTexts();
+        SetTimeSetterWindowTexts();
+    }
+
+    static void SetCommonTexts()
+    {
+        resources["GoBackText"] = "Vissza";
+        resources["SettingsText"] = "Beállítások";
+    }
+
+    static void SetMenuSideBarTexts()
+    {
+        resources["StartGameText"] = "Játék indítása";
+        resources["MoreOptionsText"] = "Több";
+        resources["JoinTournamentText"] = "Tornák";
+        resources["PlayStockfishText"] = "Játssz Stockfish-sel";
+        resources["CustomGameText"] = "Custom játék";
+    }
+
+    static void SetSettingsNames()
+    {
+        resources["AppThemeText"] = "App témája";
+        resources["BoardThemeText"] = "Tábla témája";
+        resources["PieceThemeText"] = "Bábuk témája";
+        resources["LanguageText"] = "Nyelv";
+        resources["DisableSoundsText"] = "Hangok kikapcsolása";
+        resources["FocusModeText"] = "Fókusz mód";
+        resources["AutoQueenText"] = "Automatikus királynő";
+        resources["ConfirmEachMoveText"] = "Lépések megerősítése";
+    }
+
+    static void SetSettingsValues()
+    {
+        resources["SystemAppTheme"] = "Rendszer";
+        resources["DarkAppTheme"] = "Sötét";
+        resources["LightAppTheme"] = "Világos";
+
+        resources["BrightBlueBoardTheme"] = "Világos kék";
+        resources["DeathBoardTheme"] = "Kietlen";
+        resources["FradiBoardTheme"] = "Fradi színek";
+        resources["ModernBoardTheme"] = "Modern hatású";
+        resources["PastelBlueBoardTheme"] = "Pasztel kék";
+        resources["PastelGreenBoardTheme"] = "Pasztel zöld";
+        resources["PinkWorldBoardTheme"] = "Pink mámor";
+        resources["RoyalBoardTheme"] = "Királyi";
+        resources["WoodenBoardTheme"] = "Fa hatású";
+
+        resources["SystemLanguage"] = "Rendszer";
+        resources["EnglishLanguage"] = "Angol";
+        resources["HungarianLanguage"] = "Magyar";
+    }
+
+    static void SetOptionsPopUpTexts()
+    {
+        resources["AbortText"] = "Játék elvetése";
+        resources["ResignText"] = "Feladás";
+        resources["DrawText"] = "Döntetlen felajánlása";
+    }
+
+    static void SetLookingForMatchWindowTexts()
+    {
+        resources["LookingForMatchText"] = "Meccs keresése";
+        resources["SearchingNotes"] = new List<string>
+        {
+            "Méltó ellenfélre várakozás",
+            "A kihívók horizontjának \npásztázása",
+            "Bátor jelentkezők kutatása...",
+            "Tábla előkészítése a \nkövetkező riválisnak...",
+            "Ellenfél keresése, aki \nnem adja fel azonnal...",
+            "Egy hozzád méltó elme felkutatása...",
+            "A következő áldozat kiválasztása \nfolyamatban...",
+            "Az algoritmus suttogja: \nközeledik egy kihívó",
+            "Kemény legények felkutatása...",
+            "Az ellenfeled már \nmajdnem készen áll...",
+            "Ellenfél keresése…",
+            "Kihívó vadászata…",
+            "Ellenfél beolvasása…",
+            "Rivális felkutatása…",
+            "Keresem a bátor ellenfelet…",
+            "Csata előkészítése…",
+            "Közeledik egy kihívó…",
+            "Ellenfél betöltése…",
+            "Keresem a következő áldozatot…",
+            "Ellenfél összeválogatása…",
+            "Kihívó érkezik…",
+            "Mindjárt megvan az ellenfél…"
+        };
+    }
+
+    static void SetMatchEndWindowTexts()
+    {
+        resources["MatchResultLostText"] = "Vesztettél!";
+        resources["MatchResultDrawnText"] = "Döntetlen!";
+        resources["MatchResultWonText"] = "Győztél!";
+        resources["MatchAbortedText"] = "Meccs Elvetve!";
+
+        resources["CheckmateText"] = "Sakkmatt által";
+        resources["TimedoutText"] = "Lejárt az idő";
+        resources["ResignationText"] = "Feladás végett";
+
+        resources["MutualAgreementText"] = "Közös megegyezéssel";
+        resources["StalemateText"] = "Patt helyzet";
+        resources["FiftyConsecutiveMovesTexts"] = "Ötvenlépés szabály miatt";
+        resources["SeventyFiveConsecutiveMovesTexts"] = "Hetvenötlépés szabály miatt";
+        resources["InsufficentMaterialText"] = "Lehetetlen állás miatt";
+        resources["ThreefoldRepetition"] = "Háromszori lépésismétlés miatt";
+        resources["FivefoldRepetition"] = "Ötszöri lépésismétlés miatt";
+
+        resources["PlayAgainButtonText"] = "Új játék keresése";
+        resources["QuitToMenuButtonText"] = "Kilépés a menübe";
+    }
+
+    static void SetMenuPopUpTexts()
+    {
+        resources["ProfileSettingsText"] = "Profil beállítások";
+        resources["LogoutText"] = "Kijelentkezés";
+    }
+
+    static void SetLoginPopUpTexts()
+    {
+        resources["LoginText"] = "Bejelentkezés";
+        resources["LoginNotificationText"] = "Kérlek, a folytatáshoz jelentkezz be!";
+    }
+
+    static void SetTimeSetterWindowTexts()
+    {
+        resources["ChooseTimeText"] = "Válassz játékhosszt";
+        resources["BulletText"] = "Bullet";
+        resources["BlitzText"] = "Blitz";
+        resources["RapidText"] = "Rapid"; 
+        resources["BulletOneMinText"] = "1 perc";
+        resources["BlitzThreeMinText"] = "3 perc";
+        resources["BlitzFiveMinText"] = "5 perc";
+        resources["RapidTenMinText"] = "10 perc";
+        resources["RapidThirtyMinText"] = "15 perc";
+        resources["MinuteText"] = "perc";
+    }
+}
