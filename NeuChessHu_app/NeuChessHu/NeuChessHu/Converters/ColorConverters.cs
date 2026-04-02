4a7c77c88f3a55916cdd5a174eb304b1c72c00c6
baranyaigabor <baranyaigl06@gmail.com>
2026-04-02 16:32:27 +0200

    AppThemes + helper color converter

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Converters/ColorConverters.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace NeuChessHu.Converters;
+
+internal static class ColorConverters
+{
+    internal static Color ColorFromString(string hexa) =>
+        (Color)ColorConverter.ConvertFromString(hexa);
+}
