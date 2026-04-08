f2792a4d875809abba5b858cdfa327649374c0a9
Oláh <adam.olah06@gmail.com>
2026-04-08 20:03:50 +0200

    Calendar component downloaded

---
+++ frontend/src/components/ui/calendar/CalendarHeadCell.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactiveOmit } from "@vueuse/core";
+import { CalendarHeadCell, useForwardProps } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = reactiveOmit(props, "class");
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <CalendarHeadCell
+    data-slot="calendar-head-cell"
+    :class="
+      cn(
+        'bold mt-2.5 rounded-md flex-1 font-normal text-[0.8rem] text-center',
+        props.class,
+      )
+    "
+    v-bind="forwardedProps"
+  >
+    <slot />
+  </CalendarHeadCell>
+</template>
