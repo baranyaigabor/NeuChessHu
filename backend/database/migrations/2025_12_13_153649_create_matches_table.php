825f6312d0a5efd1727b27603f2a8f6dcfc58584
Oláh <adam.olah06@gmail.com>
2026-04-10 14:14:40 +0200

    Matches table migration added

---
+++ backend/database/migrations/2025_12_13_153649_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->string("match_id")->primary();
+            
+            $table->foreignId('white_id')->constrained('users');
+            $table->foreignId('black_id')->constrained('users');
+
+            $table->string("gamemode");
+            $table->string('match_duration');
+            $table->dateTime("played_at");
+
+            $table->json("moves")->nullable();
+            $table->string("match_end_result");
+            $table->string("winner_id")->nullable();
+            $table->string("winner_time")->nullable();
+            
+            $table->foreignId("tournament_id")->nullable()->constrained();
+        });
+    }
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
