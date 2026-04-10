d771bcf7dc1e3fc6389b37a6a15c42f5208ac464
Oláh <adam.olah06@gmail.com>
2026-04-10 15:26:57 +0200

    Use controller is added

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController
+{
+    public function index()
+    {
+        $users = User::select('id', 'nickname', 'email', 'first_name', 'last_name',
+            'region', 'date_of_birth', 'is_active', 'created_at', 'updated_at' )->get();
+
+        return UserResource::collection($users->load(["whiteMatches", "blackMatches"]));#,"tournaments"
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create($data);
+        return new UserResource($user);
+    }
+
+    public function show(string $identifier)
+    {
+        $user = is_numeric($identifier)
+            ? User::where('id', (int)$identifier)->firstOrFail()
+            : User::where('nickname', $identifier)->firstOrFail();
+    
+        return new UserResource($user->load(["whiteMatches", "blackMatches"]));#, "tournaments"
+    }
+
+    public function showCurrent()
+    {
+        $user = Auth::user();
+
+        return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->validated();
+
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user)
+    {
+        return $user->delete() ? response()->noContent() : abort(500);
+    }
+}
