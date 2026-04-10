7ff414e403f83820289d40383d956f62b927bfc5
Oláh <adam.olah06@gmail.com>
2026-04-10 16:20:14 +0200

    API routes to existinng controllers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,3 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MatchesController;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource("users", UserController::class);
+Route::get('/users/{identifier}', [UserController::class, 'show']);
+Route::get('/user', [UserController::class, 'showCurrent'])
+    ->middleware('auth:sanctum');
+
+Route::apiResource("matches", MatchesController::class);
