ef84bfbdd30d5b1408782fe77c7c6af9b76c3889
Oláh <adam.olah06@gmail.com>
2026-04-10 14:21:59 +0200

    User model updated to work with new user migration

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,47 +2,65 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        "nickname",
+        "email",
+        "password",
+        "first_name",
+        "last_name",
+        "region",
+        "profile_picture",
+        "date_of_birth",
+        "is_active"
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    public function getRouteKeyName()
+    {
+        return 'nickname';
+    }
+
+    protected function profilePicture(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value ?: null,
+        );
+    }
+
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return ['password' => 'hashed'];
+    }
+
+    public function whiteMatches()
+    {
+        return $this->hasMany(Matches::class, 'white_id');
+    }
+
+    public function blackMatches()
+    {
+        return $this->hasMany(Matches::class, 'black_id');
+    }
+
+    public function tournaments(): belongsToMany
+    {
+        return $this->belongsToMany(Tournament::class, 'tournaments_user', 'tournament_name', 'user_id');
     }
 }
