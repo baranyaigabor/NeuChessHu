251bd6a1d5eab1d1407d0065fb0b005a2f957d71
Oláh <adam.olah06@gmail.com>
2026-04-15 15:14:06 +0200

    BaseMain layout component done

---
+++ frontend/src/layouts/BaseMain.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import BaseLoginHeader from '@components/layout/BaseLoginHeader.vue'
+import BaseSidebar from '@components/layout/BaseSidebar.vue'
+import BaseWelcomeBody from '@components/layout/BaseWelcomeBody.vue'
+import BaseUserBody from '@components/layout/BaseUserBody.vue'
+
+const route = useRoute()
+const showWelcomeBody = computed(() => route.name === 'welcome')
+</script>
+
+<template>
+  <BaseLoginHeader />
+  
+  <div class="flex">
+    <BaseSidebar class="mr-5" />
+
+    <main>
+      <BaseWelcomeBody v-if="showWelcomeBody"/>
+      <BaseUserBody v-else/>
+    </main>
+  </div>
+</template>
