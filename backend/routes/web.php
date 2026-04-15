a95e577e990a91a78c6f46eb2247921ecb4c9108
Oláh <adam.olah06@gmail.com>
2026-04-15 20:44:53 +0200

    Web.php desktop authorization routes added

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\DesktopAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/desktop/authorize', [DesktopAuthController::class, 'authorizeApp']);
+Route::get('/oauth/close', [DesktopAuthController::class, 'oauthClose']);
