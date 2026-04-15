a2d8790279d8fcd75c41928dd55e5e11dc2dd029
Oláh <adam.olah06@gmail.com>
2026-04-15 15:30:56 +0200

    Base login header layout done

---
+++ frontend/src/layouts/BaseLoginLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BaseLoginHeader from '@components/layout/BaseLoginHeader.vue'
+</script>
+
+<template>
+  <BaseLoginHeader />
+
+  <main class="container mx-auto">
+    <slot />
+  </main>
+</template>
