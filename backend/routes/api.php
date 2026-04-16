0fb362066a9572ef3370092282dab0dffea4afe7
baranyaigabor <baranyaigl06@gmail.com>
2026-04-16 21:55:40 +0200

    Match ready controller is added, new endpoints and channels are added as well

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\BroadcastAuthController;
 use App\Http\Controllers\Auth\DesktopAuthController;
+use App\Http\Controllers\ChatUpdateController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MatchesController;
+use App\Http\Controllers\MatchUpdateController;
+use App\Http\Controllers\QueueController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("users", UserController::class);
@@ -18,3 +22,14 @@ Route::post('/logout', [AuthController::class, 'webLogout']);
 
 Route::post('/desktop/logout', [DesktopAuthController::class, 'desktopLogout'])
     ->middleware('auth:sanctum');
+
+Route::post('/join/matchmakingqueue', [QueueController::class, 'join'])
+    ->middleware('auth:sanctum');
+Route::post('/leave/matchmakingqueue', [QueueController::class, 'leave'])
+    ->middleware('auth:sanctum');
+
+Route::post('/broadcasting/auth', [BroadcastAuthController::class, 'broadcastAuthenticate'])
+    ->middleware('auth:sanctum');
+
+Route::post('match/state-update', [MatchUpdateController::class, 'update']);
+Route::post('match/chat-update', [ChatUpdateController::class, 'update']);
--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
+Broadcast::channel('waiting-queue', function ($user) {
+    return true;
+});
+
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+
 Broadcast::channel('{player1}-{player2}-{timestamp}', function ($user, $player1, $player2, $timestamp) {
     return (int)$user->id === (int)$player1 || (int)$user->id === (int)$player2;
 });
