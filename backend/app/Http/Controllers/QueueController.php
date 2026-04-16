85849754899b494f3598736b77bc8f53b9fe3889
baranyaigabor <baranyaigl06@gmail.com>
2026-04-16 18:34:40 +0200

    Join and leave from the queue added

--- backend/app/Http/Controllers/QueueController.php
+++ backend/app/Http/Controllers/QueueController.php
@@ -11,11 +11,38 @@ class QueueController
 {
     public function __construct(private MatchService $matchMaker) {}
 
+    public function join(QueueRequest $request)
+    {
+        $this->enqueuePlayer($request->playerID, $request->matchDuration);
+    }
+
     public function enqueuePlayer(int $playerID, string $match_duration) : void
     {
         MatchmakingQueue::updateOrCreate(
             ['player_id' => $playerID],
             ['match_duration' => $match_duration, 'joined_at' => now()]
         );
+    }
+
+    public function leave(QueueRequest $request)
+    {
+        $playerId = $request->playerID;
+
+        $player = MatchmakingQueue::where('player_id', $playerId)->first();
+
+        if (!$player) 
+        {
+            return response()->json([
+                'status' => 'not-in-queue',
+                'message' => 'Player is not currently in the matchmaking queue.'
+            ]);
+        }
+
+        $player->delete();
+
+        return response()->json([
+            'status' => 'left',
+            'message' => 'Player successfully removed from the matchmaking queue.'
+        ]);
     }
 }
