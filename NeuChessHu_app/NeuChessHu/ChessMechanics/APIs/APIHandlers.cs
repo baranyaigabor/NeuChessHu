0d5a86541432ed3af8804b1255aa59083805d54d
baranyaigabor <baranyaigl06@gmail.com>
2026-04-16 22:47:04 +0200

    Extended api handlers

--- NeuChessHu_app/NeuChessHu/ChessMechanics/APIs/APIHandlers.cs
+++ NeuChessHu_app/NeuChessHu/ChessMechanics/APIs/APIHandlers.cs
@@ -1,22 +1,39 @@
 ﻿using ChessMechanics.Authentication.Session;
+using System.ComponentModel;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace ChessMechanics.APIs;
 
-public record APIHandlers(SessionDatas Session) : IDisposable
+public class APIHandlers : IDisposable
 {
     const string baseUrl = "http://backend.vm2.test/api/";
+
+    readonly SessionDatas session;
+
     HttpClient? httpClient;
 
     HttpClient HttpClient => httpClient ??= CreateHttpClient();
+
+    public APIHandlers(SessionDatas session)
+    {
+        this.session = session;
+        session.PropertyChanged += OnSessionChanged;
+    }
+
+    void OnSessionChanged(object? s, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(SessionDatas.Token))
+            HttpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", session.Token);
+    }
 
     HttpClient CreateHttpClient() => new()
     {
         Timeout = TimeSpan.FromSeconds(10),
         DefaultRequestHeaders =
         {
-            Authorization = new AuthenticationHeaderValue("Bearer", Session.Token),
+            Authorization = new AuthenticationHeaderValue("Bearer", session.Token),
             Accept = { new MediaTypeWithQualityHeaderValue("application/json") }
         }
     };
@@ -26,15 +43,15 @@ public record APIHandlers(SessionDatas Session) : IDisposable
 
     public async Task HttpJoinMatchmakingQueueAsync(string matchDuration) =>
         await HttpClient.PostAsJsonAsync($"{baseUrl}join/matchmakingqueue",
-            new { playerID = Session.UserID, matchDuration }).ConfigureAwait(false);
+            new { playerID = session.UserID, matchDuration }).ConfigureAwait(false);
 
     public async Task HttpLeaveMatchmakingQueueAsync() =>
         await HttpClient.PostAsJsonAsync($"{baseUrl}leave/matchmakingqueue",
-            new { playerID = Session.UserID }).ConfigureAwait(false);
+            new { playerID = session.UserID }).ConfigureAwait(false);
 
     internal async Task HttpLogoutAsync() =>
         await HttpClient.PostAsJsonAsync($"{baseUrl}desktop/logout",
-            new { playerID = Session.UserID }).ConfigureAwait(false);
+            new { playerID = session.UserID }).ConfigureAwait(false);
 
     internal async Task<string> HttpAuthenticateAsync(string socketID, string channel) =>
         await (await HttpClient.PostAsJsonAsync($"{baseUrl}broadcasting/auth",
@@ -43,6 +60,9 @@ public record APIHandlers(SessionDatas Session) : IDisposable
     internal async Task<string> HttpMatchReadyAsync(string channel) =>
         await (await HttpClient.PostAsJsonAsync($"{baseUrl}match/ready",
             new { channel_name = channel })).Content.ReadAsStringAsync();
+
+    internal async Task<string?> HttpGetPendingChannelAsync() =>
+        await HttpClient.GetStringAsync($"{baseUrl}match/pendingchannel");
 
     public void Dispose() =>
         httpClient?.Dispose();
