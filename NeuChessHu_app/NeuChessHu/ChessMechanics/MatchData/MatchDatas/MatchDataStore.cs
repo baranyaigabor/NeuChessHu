2e81ee2542f3a2d3e7c89a97ea90e2208249e84a
baranyaigabor <baranyaigl06@gmail.com>
2026-04-16 23:26:34 +0200

    MatchDataStore added, instantiating all the models

---
+++ NeuChessHu_app/NeuChessHu/ChessMechanics/MatchData/MatchDatas/MatchDataStore.cs
@@ -0,0 +1,25 @@
+﻿using ChessMechanics.ChessBoard.Definitions;
+using ChessMechanics.MatchData.MatchDatas.Models;
+
+namespace ChessMechanics.MatchData.MatchDatas;
+
+public class MatchDataStore
+{
+    public string? MatchChannel { get; set; }
+
+    public MatchPoints MatchPoints { get; }
+    public MatchState MatchState { get; }
+    public Dictionary<Side, PlayerDataStore> PlayerDatas { get; }
+    public ChatMessages ChatMessageList { get; set; }
+
+    public MatchDataStore()
+    {
+        MatchPoints = MatchPoints.CreateMatchPoints();
+        MatchState = MatchState.CreateMatchState();
+
+        PlayerDatas = Enum.GetValues<Side>().Cast<Side>().Take(2)
+            .ToDictionary(x => x, x => PlayerDataStore.CreatePlayerDataStore());
+
+        ChatMessageList = ChatMessages.CreateChatMessages();
+    }
+}
