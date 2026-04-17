5a9dcb7b1c5bfec86539f003be5bb6586cc3a2e7
baranyaigabor <baranyaigl06@gmail.com>
2026-04-17 12:47:37 +0200

    MatchPointsDTO added

---
+++ NeuChessHu_app/NeuChessHu/ChessMechanics/MatchData/MatchDatas/DataTransferObjects/MatchStateDTO.cs
@@ -0,0 +1,26 @@
+﻿using ChessMechanics.ChessBoard.ChessPieces;
+using ChessMechanics.ChessBoard.Definitions;
+using ChessMechanics.MatchData.MatchDatas.Models.DomainModels;
+using System.Collections.ObjectModel;
+
+namespace ChessMechanics.MatchData.MatchDatas.DataTransferObjects;
+
+public class MatchStateDTO
+{
+    public string? MatchID { get; set; }
+
+    public Side? CurrentSide { get; set; }
+    public ChessPiece[,]? PieceMatrix { get; set; }
+
+    public string? MatchDuration { get; set; }
+    public ObservableCollection<SANNotationRow>? Notations { get; set; }
+
+    public bool? HasWKMoved { get; set; } 
+    public bool? HasWRAMoved { get; set; }
+    public bool? HasWRHMoved { get; set; }
+    public bool? HasBKMoved { get; set; } 
+    public bool? HasBRAMoved { get; set; }
+    public bool? HasBRHMoved { get; set; }
+
+    public Tuple<int, int>? EnPassantTarget { get; set; }
+}
