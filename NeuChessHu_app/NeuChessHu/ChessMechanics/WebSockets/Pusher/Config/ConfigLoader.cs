ed99ca8a289c176d8509076025d1a7eb073d4c5b
baranyaigabor <baranyaigl06@gmail.com>
2026-04-17 11:23:45 +0200

    PusherClientService

---
+++ NeuChessHu_app/NeuChessHu/ChessMechanics/WebSockets/Pusher/Config/ConfigLoader.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ChessMechanics.WebSocketss.Pusher.Config;
+
+internal static class ConfigLoader
+{
+    internal static IConfigurationRoot Load(string configFile) =>
+        new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
+                                  .AddJsonFile(configFile, optional: true, reloadOnChange: true)
+                                  .Build();
+}
---
+++ NeuChessHu_app/NeuChessHu/ChessMechanics/WebSockets/Pusher/PusherClientService.cs
@@ -0,0 +1,30 @@
+﻿using ChessMechanics.WebSocketss.Pusher.Config;
+using System.Reactive.Linq;
+using Websocket.Client;
+
+namespace ChessMechanics.WebSockets.Pusher;
+
+public class PusherClientService
+{
+    readonly WebsocketClient webSocketClient;
+
+    bool isManualDisconnect = false;
+
+    public event Action? OnConnected;
+    public event Action? OnDisconnected;
+
+    public PusherClientService(PusherConfig config)
+    {
+        webSocketClient = new WebsocketClient(
+            new($"ws://10.1.3.33:6001/app/{config.AppKey}?protocol=7&client=dotnet"))
+        {
+            ReconnectTimeout = TimeSpan.FromSeconds(60)
+        };
+
+        webSocketClient.DisconnectionHappened.Subscribe(_ =>
+        {
+            if (!isManualDisconnect)
+                OnDisconnected?.Invoke();
+        });
+    }
+}
