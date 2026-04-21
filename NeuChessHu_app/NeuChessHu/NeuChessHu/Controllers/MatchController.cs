ca10d3d8f436623e65465d851463f5bbddb83ee9
baranyaigabor <baranyaigl06@gmail.com>
2026-04-21 16:41:14 +0200

    LookingForMatchService is added, and MatchController got the UIContext as a property for safety reasons

--- NeuChessHu_app/NeuChessHu/NeuChessHu/Controllers/MatchController.cs
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Controllers/MatchController.cs
@@ -9,52 +9,52 @@ namespace NeuChessHu.Controllers;
 public class MatchController(PusherClientService Pusher, MatchDataStore MatchDataStore,
     ClockHandler clocks)
 {
+    internal SynchronizationContext UIContext { get; set; }
+
     public Func<Task>? OnPlayAgain { get; internal set; }
 
     internal async Task SubscribeMatchChannelsAsync(Action? onInitialized = null)
     {
-        SynchronizationContext uiContext = SynchronizationContext.Current!;
-
         Pusher.Bind<InitializerDTO>("match-start", async x =>
         {
             await MatchDataStore.InitializeAsync(x, clocks);
-            uiContext.Post(_ => onInitialized?.Invoke(), null);
+            UIContext.Post(_ => onInitialized?.Invoke(), null);
         });
 
         Pusher.Bind<MatchPointsDTO>("match-points", async x =>
         {
             await MatchDataStore.Initialize;
 
-            uiContext.Post(_ => Patcher.PatchMatchPoints(x, MatchDataStore.MatchPoints), null);
+            UIContext.Post(_ => Patcher.PatchMatchPoints(x, MatchDataStore.MatchPoints), null);
         });
 
         Pusher.Bind<MatchStateDTO>("match-state", async x =>
         {
             await MatchDataStore.Initialize;
 
-            uiContext.Post(_ =>
-                Patcher.PatchMatchState(x, MatchDataStore, MatchDataStore.MatchState, uiContext), null);
+            UIContext.Post(_ =>
+                Patcher.PatchMatchState(x, MatchDataStore, MatchDataStore.MatchState, UIContext), null);
         });
 
         Pusher.Bind<PlayerDatasDTO>("player-datas", async x =>
         {
             await MatchDataStore.Initialize;
 
-            uiContext.Post(_ => Patcher.PatchPlayerDatas(x, MatchDataStore.PlayerDatas, uiContext), null);
+            UIContext.Post(_ => Patcher.PatchPlayerDatas(x, MatchDataStore.PlayerDatas, UIContext), null);
         });
 
         Pusher.Bind<ChatMessagesDTO>("chat-messages", async x =>
         {
             await MatchDataStore.Initialize;
 
-            uiContext.Post(_ => Patcher.PatchChatMessages(x, MatchDataStore.ChatMessageList, uiContext), null);
+            UIContext.Post(_ => Patcher.PatchChatMessages(x, MatchDataStore.ChatMessageList, UIContext), null);
         });
 
         Pusher.Bind<ClocksDTO>("clocks", async x =>
         {
             await MatchDataStore.Initialize;
 
-            uiContext.Post(_ => Patcher.PatchClocks(x, clocks), null);
+            UIContext.Post(_ => Patcher.PatchClocks(x, clocks), null);
         });
 
         Pusher.Bind<object>("match-start-failed", async _ =>
---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Services/MatchServices/LookingForMatchService.cs
@@ -0,0 +1,104 @@
+﻿using ChessMechanics.APIs;
+using ChessMechanics.Authentication.Session;
+using ChessMechanics.MatchData.MatchDatas;
+using ChessMechanics.MatchData.MatchDatas.DataTransferObjects;
+using ChessMechanics.WebSockets.ChessEngine;
+using ChessMechanics.WebSockets.Pusher;
+using Microsoft.Extensions.DependencyInjection;
+using NeuChessHu.Collections.Contexts;
+using NeuChessHu.Controllers;
+using NeuChessHu.UserSettings;
+using System.Windows;
+
+namespace NeuChessHu.Services.MatchServices;
+
+public record LookingForMatchService(IServiceProvider Provider, SessionDatas Session,
+    APIHandlers HttpClients, PusherClientService Pusher, ChessEngineClientService ChessEngineClient,
+    BindableSettings Settings) : IAsyncDisposable
+{
+    bool isLookingForMatch = false;
+
+    IServiceScope? matchScope;
+    Action? onConnectedHandler;
+
+    internal IServiceScope? MatchScope => matchScope;
+
+    public Action? OnSwitchToMatch { get; internal set; }
+
+    public async Task LookingForMatchAsync()
+    {
+        Pusher.Bind<ChannelAssignmentDTO>("assign-channel", HandleAssignChannelAsync);
+        await StartWebsocketsAsync();
+    }
+
+    async Task HandleAssignChannelAsync(ChannelAssignmentDTO channelAssignmentDTO)
+    {
+        if (channelAssignmentDTO.PlayerID != Session.UserID.ToString())
+            return;
+
+        Pusher.Unbind("assign-channel");
+        await HttpClients.HttpLeaveMatchmakingQueueAsync();
+
+        isLookingForMatch = false;
+
+        matchScope ??= Provider.CreateScope();
+
+        matchScope!.ServiceProvider.GetRequiredService<MatchContext>();
+
+        await Application.Current.Dispatcher.InvokeAsync(async () =>
+            await SubscribeMatchChannelAsync(channelAssignmentDTO.Channel));
+    }
+
+    async Task StartWebsocketsAsync()
+    {
+        if (onConnectedHandler is not null)
+        {
+            Pusher.OnConnected -= onConnectedHandler;
+            onConnectedHandler = null;
+        }
+
+        isLookingForMatch = true;
+
+        onConnectedHandler = async () =>
+        {
+            if (isLookingForMatch)
+                await HttpClients.HttpJoinMatchmakingQueueAsync(Settings.LastMatchDuration);
+        };
+        Pusher.OnConnected += onConnectedHandler;
+
+        await Pusher.InitializePusherClientAsync();
+        await ChessEngineClient.InitializeChessEngineClientAsync();
+    }
+
+    async Task SubscribeMatchChannelAsync(string channel)
+    {
+        matchScope!.ServiceProvider.GetRequiredService<MatchDataStore>().MatchChannel = $"private-{channel}";
+
+        MatchController matchController = matchScope!.ServiceProvider.GetRequiredService<MatchController>();
+
+        matchController.UIContext = SynchronizationContext.Current!;
+        await matchController.SubscribeMatchChannelsAsync(OnSwitchToMatch);
+
+        await Pusher.SubscribeMatchChannelAsync($"private-{channel}");
+    }
+
+    internal async Task DisposeMatchAsync()
+    {
+        isLookingForMatch = false;
+
+        if (onConnectedHandler is not null)
+        {
+            Pusher.OnConnected -= onConnectedHandler;
+            onConnectedHandler = null;
+        }
+
+        await Pusher.StopPusherAsync();
+        await ChessEngineClient.DisconnectChessEngineAsync();
+
+        matchScope?.Dispose();
+        matchScope = null;
+    }
+
+    public async ValueTask DisposeAsync() =>
+        await DisposeMatchAsync();
+}
