d6d84f6aaa7e2f977a8c58faae79de9fd4d3dc8a
baranyaigabor <baranyaigl06@gmail.com>
2026-04-22 10:04:17 +0200

    SettingServices added to the DI

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Bootstrap/Dependencies/AppServiceRegistrations/SettingServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NeuChessHu.Properties;
+using NeuChessHu.UserSettings;
+using NeuChessHu.ViewModels.Overlays.SettingsPopUp;
+
+namespace NeuChessHu.Bootstrap.Dependencies.AppServiceRegistrations;
+
+internal static class SettingServices
+{
+    internal static IServiceCollection AddSettings(this IServiceCollection services) =>
+        services.AddSingleton(Settings.Default)
+                .AddSingleton<BindableSettings>()
+                .AddTransient<SettingsPopUpViewModel>();
+}
