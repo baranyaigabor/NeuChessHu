639dbe37e1ed82555ec6224900925fe45326d944
Oláh <adam.olah06@gmail.com>
2026-04-22 08:44:46 +0200

    Styles SetCursorOnButtons method added

---
+++ NeuChessHu_app/NeuChessHu/NeuChessHu/Resources/Styles/Styles.cs
@@ -0,0 +1,26 @@
+using System.Drawing.Printing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace NeuChessHu.Resources.Styles;
+
+internal static class Styles
+{
+    readonly static ResourceDictionary resources = Application.Current.Resources;
+
+    internal static void MergeStyles()
+    {
+        SetCursorOnButtons();
+    }
+
+    static void SetCursorOnButtons()
+    {
+        Cursor cursorOnButtons = Cursors.Hand;
+        resources.Add("CursorOnButtons", cursorOnButtons);
+    }
+}
