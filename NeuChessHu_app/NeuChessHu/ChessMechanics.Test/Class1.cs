1773ca9186d8b799e5219369f176edc3feffd047
baranyaigabor <baranyaigl06@gmail.com>
2026-04-22 20:07:08 +0200

    ChessMechanics.Test tester library is added

---
+++ NeuChessHu_app/NeuChessHu/ChessMechanics.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessMechanics.Test
+{
+    internal class Class1
+    {
+    }
+}
