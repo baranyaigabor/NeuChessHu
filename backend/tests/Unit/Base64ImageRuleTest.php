61bb541924d7f3295da6fcbc26cf9d730ffc1da7
Oláh <adam.olah06@gmail.com>
2026-04-22 17:34:55 +0200

    Base64ImageRuleTest testJpegPrefixIsAcceptedFormat method added

--- backend/tests/Unit/Base64ImageRuleTest.php
+++ backend/tests/Unit/Base64ImageRuleTest.php
@@ -62,4 +62,25 @@ class Base64ImageRuleTest extends TestCase
         $this->assertNotEmpty($errors);
         $this->assertStringContainsString('2MB', $errors[0]);
     }
+
+    public function testValidBase64ButNotImageFails(): void
+    {
+        $value = 'data:image/png;base64,' . base64_encode('this is not an image');
+        $errors = $this->validate($value);
+        $this->assertNotEmpty($errors);
+    }
+
+    public function testJpegPrefixIsAcceptedFormat(): void
+    {
+        $jpegData = "\xFF\xD8\xFF\xE0\x00\x10JFIF\x00\x01\x01\x00\x00\x01\x00\x01\x00\x00"
+                  . "\xFF\xDB\x00C\x00\x08\x06\x06\x07\x06\x05\x08\x07\x07\x07\t\t\x08\n\x0c\x14"
+                  . "\r\x0c\x0b\x0b\x0c\x19\x12\x13\x0f\x14\x1d\x1a\x1f\x1e\x1d\x1a\x1c\x1c $.',"
+                  . '"#\x1c\'25=82<.342\x1c\'...\xFF\xD9';
+        $value = 'data:image/jpeg;base64,' . base64_encode($jpegData);
+        $errors = $this->validate($value); 
+        
+        if (!empty($errors)) {
+            $this->assertStringNotContainsString('JPEG or PNG', $errors[0]);
+        }
+    }
 }
