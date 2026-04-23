24b3a2670c22112e8ba3217bae154fe951ead33c
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 17:54:18 +0200

    EmailInput is added for SignUp

---
+++ frontend/src/components/ui/signup-card/emailInput.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: ""
+  }
+})
+
+const emit = defineEmits(['email-change'])
+const { t } = useI18n()
+
+function onEmailChange(event) {
+  const value = event.target.value
+  emit('email-change', value)
+}
+</script>
+
+<template>
+    <div class="gx-0 loginSep container-fluid">
+        <p class="text-(--TextBrush)">{{ t('common.email') }}: *</p>
+        <input id="loginEmail" 
+               @change="onEmailChange" 
+               :value="props.value" 
+               class="w-full m-0 bg-(--ButtonBrush) text-(--FieldTextBrush) placeholder:text-(--FieldTextBrush) placeholder:opacity-60 p-1 ps-2 rounded-[5px] border border-(--BorderBrush) shadow-[inset_0_2px_5px_var(--InsetShadowBrush)]" 
+               placeholder="example@example.com" 
+               type="text">
+    </div>
+</template>
