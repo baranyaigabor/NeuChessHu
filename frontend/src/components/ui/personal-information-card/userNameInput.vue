d3e8fb86e0d7082d0256633929bb309a924d5972
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 19:47:29 +0200

    UserNameInput for Personal Informations

---
+++ frontend/src/components/ui/personal-information-card/userNameInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: ""
+  }
+})
+
+const emit = defineEmits(['usernameChange'])
+const { t } = useI18n()
+
+function onUsernameChange(event) 
+{
+  const value = event.target.value
+  emit('usernameChange', value)
+}
+</script>
+
+<template>
+    <div class="col">
+        <label for="username">{{ t('common.username') }}: *</label><br>
+        <input id="username" @change="onUsernameChange" :value="props.value" class="w-full m-0 bg-(--ButtonBrush) text-(--FieldTextBrush) placeholder:text-(--FieldTextBrush) placeholder:opacity-60 p-1 ps-2 rounded-[5px] border border-(--BorderBrush) shadow-[inset_0_2px_5px_var(--InsetShadowBrush)] !focus:bg-transparent focus:outline-none" 
+               type="text" 
+               :placeholder="t('common.username')" 
+               required 
+               minlength="3" 
+               maxlength="14">
+    </div>
+</template>
