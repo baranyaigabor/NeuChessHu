f25ed0d17ebe535a82f50309e5548b3420692d05
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 20:57:13 +0200

    Region is added for Personal Informations

--- frontend/src/components/ui/personal-information-card/index.js
+++ frontend/src/components/ui/personal-information-card/index.js
@@ -1,3 +1,4 @@
 export { default as UserName } from './userName.vue'
 export { default as UserNameInput } from './userNameInput.vue'
 export { default as UserDateOfBirthInput } from './userDateOfBirth.vue'
+export { default as UserRegionInput } from './userRegion.vue'
