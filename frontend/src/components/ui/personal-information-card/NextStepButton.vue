c88eba5da98f0642fafd7f09d7017d9cc0823204
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 21:12:25 +0200

    NextStepButton is added for Personal Informations

---
+++ frontend/src/components/ui/personal-information-card/NextStepButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const emit = defineEmits(['submit'])
+const { t } = useI18n()
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+    <div class="container-fluid justify-content-end d-flex">
+        <a href="#" id="submit_BTN" @click.prevent="!props.disabled && emit('submit')" class="btn mt-4 w-[5.5rem] bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">{{ t('common.next') }}</a>
+    </div>
+</template>
