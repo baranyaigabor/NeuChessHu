8b4e484b2b5441cd233a3d9a0662e74947502f76
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 18:02:16 +0200

    SignUpNextStepButton is added

--- frontend/src/components/ui/signup-card/index.js
+++ frontend/src/components/ui/signup-card/index.js
@@ -1,2 +1,3 @@
 export { default as EmailSignInInput } from './emailInput.vue'
 export { default as PasswordSignInInput } from './passwordInput.vue'
+export { default as SignUpNextStepButton } from './singUpNextStepButton.vue'
---
+++ frontend/src/components/ui/signup-card/singUpNextStepButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const emit = defineEmits(['submit'])
+const { t } = useI18n()
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+    <div class="container-fluid justify-content-end d-flex p-0">
+        <a id="singup_BTN"
+           @click.prevent="!props.disabled && emit('submit')" 
+           class="btn w-18 bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">
+           {{ t('common.next') }}
+        </a>
+    </div>
+</template>
