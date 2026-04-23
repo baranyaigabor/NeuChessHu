48a313606292395658fd5eab116cf6ca8c74db15
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 20:14:56 +0200

    UserName fields container is added for the Personal Informations

---
+++ frontend/src/components/ui/personal-information-card/userName.vue
@@ -0,0 +1,23 @@
+<script setup>
+import UserFirstName from './userFirstNameInput.vue';
+import UserLastName from './userLastNameInput.vue';
+
+const props = defineProps({
+  firstName: {
+    type: String,
+    default: ""
+  },
+  lastName: {
+    type: String,
+    default: ""
+  }
+})
+
+const emit = defineEmits(['firstNameChange', 'lastNameChange'])
+</script>
+
+<template>
+    <UserFirstName :value="firstName" @firstNameChange="emit('firstNameChange', $event)"/>
+
+    <UserLastName :value="lastName" @lastNameChange="emit('lastNameChange', $event)"/>
+</template>
