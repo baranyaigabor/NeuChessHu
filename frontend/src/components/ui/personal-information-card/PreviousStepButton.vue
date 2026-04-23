7024e6c822ba38925be893f273082481e37a524a
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 21:27:39 +0200

    PreviousStepButton is added for Personal Informations

---
+++ frontend/src/components/ui/personal-information-card/PreviousStepButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const emit = defineEmits(['submit'])
+const { t } = useI18n()
+</script>
+
+<template>
+    <div class="container-fluid justify-content-start d-flex">
+        <a href="#" id="submit_BTN" @click.prevent="emit('submit')" class="btn mt-4 w-[5.5rem] bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">{{ t('common.previous') }}</a>
+    </div>
+</template>
