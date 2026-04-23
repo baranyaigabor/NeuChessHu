1fb6c0984fcb77f60a74e43df94d7bdb3d0dd48c
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 22:48:11 +0200

    SignIn page is added

---
+++ frontend/src/pages/signin/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BaseLoginLayout from '@layouts/BaseLoginLayout.vue'
+import BaseSingInCard from '@components/layout/BaseSignInCard.vue'
+</script>
+
+<template>
+    <BaseLoginLayout>
+        <BaseSingInCard />
+    </BaseLoginLayout>
+</template>
+
+<style lang="css">
+</style>
+
+<route lang="yaml">
+name: signin
+meta:
+    title: Sign In
+</route>
