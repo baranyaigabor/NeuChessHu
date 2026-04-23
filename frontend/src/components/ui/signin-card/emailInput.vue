768ab56ef3898a1a57de95195388b97357500aef
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 18:49:18 +0200

    EmailInput for SigIn is added

---
+++ frontend/src/components/ui/signin-card/emailInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const emit = defineEmits(['email-change'])
+const { t } = useI18n()
+
+function onEmailChange(event) {
+  const value = event.target.value
+  emit('email-change', value)
+}
+</script>
+
+<template>
+    <div class="gx-0 loginSep container-fluid">
+        <p class="text-(--TextBrush)">{{ t('common.email') }}:</p>
+        <input id="loginEmail"
+               @change="onEmailChange" 
+               class="w-full bg-(--ButtonBrush) text-(--FieldTextBrush) placeholder:text-(--FieldTextBrush) placeholder:opacity-60 m-0 p-1 ps-2 rounded-[5px] border border-(--BorderBrush)! shadow-[inset_0_2px_5px_var(--InsetShadowBrush)]" 
+               placeholder="example@example.com" 
+               type="text">
+    </div>
+</template>
+
+<style lang="css">
+</style>
---
+++ frontend/src/components/ui/signin-card/index.js
@@ -0,0 +1,1 @@
+export { default as EmailSignInInput } from './emailInput.vue'
