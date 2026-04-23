a86e13c1871033622048cb09d34099b1b63d5377
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 22:50:30 +0200

    SignUp page is added

---
+++ frontend/src/pages/signup/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BaseLoginLayout from '@layouts/BaseLoginLayout.vue'
+import BaseSignUpCard from '@components/layout/BaseSignUpCard.vue'
+</script>
+
+<template>
+    <BaseLoginLayout>
+    <BaseSignUpCard />
+    </BaseLoginLayout>
+</template>
+
+<style lang="css">
+</style>
+
+<route lang="yaml">
+name: signup
+meta:
+    title: Sign Up
+</route>
