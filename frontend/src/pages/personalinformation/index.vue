a8f7be3d4031895ed8542242c0107e3197252c44
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 22:51:37 +0200

    PersonalInformation page is added

---
+++ frontend/src/pages/personalinformation/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BaseLoginLayout from '@layouts/BaseLoginLayout.vue'
+import BasePersonalInformationCard from '@components/layout/BasePersonalInformation.vue'
+</script>
+
+<template>
+    <BaseLoginLayout>
+        <BasePersonalInformationCard />
+    </BaseLoginLayout>
+</template>
+
+<route lang="yaml">
+name: personalinformation
+meta:
+    title: Personal Information
+</route>
