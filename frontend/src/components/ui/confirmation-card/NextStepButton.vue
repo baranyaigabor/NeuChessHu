392002e58bdd9e036f7b77f409ee91855b1db85b
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 22:59:49 +0200

    NextStepButton for Confirmation is added

---
+++ frontend/src/components/ui/confirmation-card/NextStepButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useI18n } from '@utils/i18n'
+
+const emit = defineEmits(['submit'])
+const { t } = useI18n()
+</script>
+
+<template>
+    <div class="container-fluid d-flex justify-content-end p-0">
+        <a href="#" 
+           id="submit_BTN" 
+           @click.prevent="emit('submit')" 
+           class="btn mt-4 w-[6rem] bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">
+           {{ t('common.confirm') }}</a>
+    </div>
+</template>
