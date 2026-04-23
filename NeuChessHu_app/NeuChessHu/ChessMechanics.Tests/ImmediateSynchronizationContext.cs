eccade751e3659f0aef062131a2027b500fdc9e5
baranyaigabor <baranyaigl06@gmail.com>
2026-04-23 08:20:17 +0200

    Odd files deleted

--- NeuChessHu_app/NeuChessHu/ChessMechanics.Tests/ImmediateSynchronizationContext.cs
+++
@@ -1,6 +0,0 @@
-namespace ChessMechanics.Tests;
-
-internal sealed class ImmediateSynchronizationContext : SynchronizationContext
-{
-    public override void Post(SendOrPostCallback d, object? state) => d(state);
-}
