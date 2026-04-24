3db407e695457e1564bb51a1c98c10046b5dac08
Oláh <adam.olah06@gmail.com>
2026-04-24 10:57:49 +0200

    App.vue updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+import { onMounted, onBeforeUnmount } from 'vue'
+import { useUserStore } from '@stores/UserStore'
+
+onMounted(() => {
+    window.addEventListener('beforeunload', handleUnload)
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener('beforeunload', handleUnload)
+})
+
+function handleUnload()
+{
+    const token = useUserStore().token
+
+    if(token !== null)
+    {
+        navigator.sendBeacon(`/api/logout?token=${token}`)
+    }
+}
+</script>
 
 <template>
-  <RouterView />
+    <div class="min-h-screen bg-(--WindowBrush) text-(--TextBrush)">
+        <RouterView />
+    </div>
 </template>
