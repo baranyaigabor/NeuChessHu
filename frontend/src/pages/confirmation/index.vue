54b8a6a851c9e37535e8e99e1fd4a56451a62cf9
baranyaigabor <baranyaigl06@gmail.com>
2026-04-24 09:41:45 +0200

    Confirmation page is added

---
+++ frontend/src/pages/confirmation/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BaseLoginLayout from '@layouts/BaseLoginLayout.vue'
+import BaseConfirmationsCard from '@components/layout/BaseConfirmationsCard.vue'
+</script>
+
+<template>
+    <BaseLoginLayout>
+        <BaseConfirmationsCard />
+    </BaseLoginLayout>
+</template>
+
+<route lang="yaml">
+name: confirminformation
+meta:
+    title: Confirmations
+</route>
