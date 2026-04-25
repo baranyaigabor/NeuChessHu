b6977c3a08c5710c5b89584bc8b3f0b3108f0f43
baranyaigabor <baranyaigl06@gmail.com>
2026-04-25 15:48:33 +0200

    Light/Dark theme toggler class added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,11 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { plugin, defaultConfig } from '@formkit/vue'
 import {router} from '@/router'
 import App from '@/App.vue'
+import { applySavedTheme } from '@/utils/theme'
 
 import '@assets/main.css'
+
+applySavedTheme()
 
 createApp(App)
   .use(createPinia().use(piniaPluginPersistedstate))
