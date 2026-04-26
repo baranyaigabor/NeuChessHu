20bf454cef382c8249268749cfdfbe16a968ec9c
Oláh <adam.olah06@gmail.com>
2026-04-26 15:43:36 +0200

    ThemeToggleButton component script done

---
+++ frontend/src/components/ui/navbar/ThemeToggleButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+import { useI18n } from '@utils/i18n'
+import { navIconButtonClass, navMenuButtonClass, navSvgClass } from './navActionClasses.mjs'
+
+const props = defineProps({
+    isDarkTheme: {
+        type: Boolean,
+        default: false
+    },
+    menu: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const emit = defineEmits(['toggle'])
+const { t } = useI18n()
+
+const buttonClass = computed(() =>
+    props.menu ? navMenuButtonClass : navIconButtonClass
+)
+
+const label = computed(() =>
+    props.isDarkTheme 
+        ? t('nav.lightMode') 
+        : t('nav.darkMode')
+)
+</script>
+
+<template>
+    
+</template>
