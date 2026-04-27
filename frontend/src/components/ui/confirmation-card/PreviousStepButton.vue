7e31f8f8887273d709694d50020c0746e9c0d33a
baranyaigabor <baranyaigl06@gmail.com>
2026-04-27 22:13:21 +0200

    Button's got their style fixed

--- frontend/src/components/ui/confirmation-card/PreviousStepButton.vue
+++ frontend/src/components/ui/confirmation-card/PreviousStepButton.vue
@@ -7,7 +7,7 @@ const { t } = useI18n()
 
 <template>
     <div class="container-fluid d-flex justify-content-start p-0">
-        <a href="#" @click.prevent="emit('submit')" class="btn mt-4 w-26 bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">
+        <a href="#" @click.prevent="emit('submit')" class="btn mt-4 w-24 bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!">
             {{ t('common.previous') }}
         </a>
     </div>
--- frontend/src/components/ui/personal-information-card/NextStepButton.vue
+++ frontend/src/components/ui/personal-information-card/NextStepButton.vue
@@ -15,8 +15,8 @@ const props = defineProps({
 <template>
     <div class="container-fluid justify-content-end d-flex">
         <a href="#" id="submit_BTN" @click.prevent="!props.disabled && emit('submit')" :aria-disabled="props.disabled"
-           :class="['btn mt-4 w-26 bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!', props.disabled ? 'disabled opacity-50 cursor-not-allowed' : '']">
-           {{ t('common.next') }}
-          </a>
+            :class="[ 'btn mt-4 w-26 bg-(--ButtonBrush)! text-(--TextBrush)! border border-(--BorderBrush)!',
+              props.disabled ? 'disabled opacity-50 cursor-not-allowed' : '']">
+           {{ t('common.next') }}</a>
     </div>
 </template>
