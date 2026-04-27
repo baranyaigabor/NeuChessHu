e4868d34e9a9ab0e0607520155c0e3ff372f096c
Oláh <adam.olah06@gmail.com>
2026-04-27 17:03:15 +0200

    User page components folder export file added

---
+++ frontend/src/components/ui/public-infos/index.js
@@ -0,0 +1,4 @@
+export { default as Infos} from "./PersonalInfos.vue";
+export { default as MatchInfos } from "./MatchInfos.vue";
+export { default as PersonalStat } from "./PersonalStats.vue";
+export { default as ChessStat} from "./ChessStats.vue";
